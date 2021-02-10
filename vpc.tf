67b69e01b3f7f4bfcc24345d0d8aba943f9caf93
Thomas Altree <altree.thomas@googlemail.com>
2021-02-10 01:18:01 +0000

    Add nat_gateway and private subnets

--- vpc.tf
+++ vpc.tf
@@ -20,6 +20,19 @@ resource "aws_subnet" "interview_public_subnets" {
   }
 }
 
+resource "aws_subnet" "interview_private_subnets" {
+  count = length(local.public_subnets)
+
+  cidr_block                = element(local.private_subnets, count.index)
+  vpc_id                    = aws_vpc.interview_vpc.id
+  availability_zone         = element(local.azs, count.index)
+
+  tags = {
+    Name = "interview-private-subnet-${element(local.azs, count.index)}"
+    Type = "Private"
+  }
+}
+
 resource "aws_internet_gateway" "interview_igw" {
   vpc_id = aws_vpc.interview_vpc.id
 
@@ -37,7 +50,7 @@ resource "aws_route_table" "interview_igw_rtb" {
   }
 
   tags = {
-    Name = "internet-igw-rtb"
+    Name = "interview-igw-rtb"
   }
 }
 
@@ -46,4 +59,39 @@ resource "aws_route_table_association" "interview_public_subnets" {
 
   subnet_id       = element(aws_subnet.interview_public_subnets.*.id, count.index)
   route_table_id  = aws_route_table.interview_igw_rtb.id
+}
+
+resource "aws_eip" "interview_nat_gw_eip" {
+  tags = {
+    Name = "interview-nat-gw-eip"
+  }
+}
+
+resource "aws_nat_gateway" "interview_nat_gw" {
+  allocation_id   = aws_eip.interview_nat_gw_eip.id
+  subnet_id       = aws_subnet.interview_private_subnets[0].id
+
+  tags = {
+    Name = "interview-nat-gw"
+  }
+}
+
+resource "aws_route_table" "interview_nat_gw_rtb" {
+  vpc_id = aws_vpc.interview_vpc.id
+
+  route {
+    cidr_block      = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.interview_nat_gw.id
+  }
+
+  tags = {
+    Name = "interview-nat-gw-rtb"
+  }
+}
+
+resource "aws_route_table_association" "interview_private_subnets" {
+  count = length(local.private_subnets)
+
+  subnet_id       = element(aws_subnet.interview_private_subnets.*.id, count.index)
+  route_table_id  = aws_route_table.interview_nat_gw_rtb.id
 }
