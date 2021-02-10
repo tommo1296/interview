a805d2df33d3384e9d78156df49dab1602bf5c1e
Thomas Altree <altree.thomas@googlemail.com>
2021-02-10 02:18:03 +0000

    Associate interview-web with target_group

--- ec2.tf
+++ ec2.tf
@@ -41,3 +41,11 @@ resource "aws_lb_target_group" "interview_web_tg" {
   protocol  = "HTTP"
   vpc_id    = aws_vpc.interview_vpc.id
 }
+
+resource "aws_lb_target_group_attachment" "interview_web_tg_instances" {
+  count = length(aws_instance.interview_web)
+
+  target_group_arn  = aws_lb_target_group.interview_web_tg.arn
+  target_id         = element(aws_instance.interview_web.*.id, count.index)
+  port              = 80
+}
