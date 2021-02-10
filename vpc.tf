1d4eb0488e637cd6d0b512193ebadf334318a9ab
Thomas Altree <altree.thomas@googlemail.com>
2021-02-10 00:20:19 +0000

    Add internet gateway

--- vpc.tf
+++ vpc.tf
@@ -41,3 +41,39 @@ resource "aws_subnet" "interview_public_subnet_c" {
     Type = "Public"
   }
 }
+
+resource "aws_internet_gateway" "interview_igw" {
+  vpc_id = aws_vpc.interview_vpc.id
+
+  tags = {
+    Name = "interview-igw"
+  }
+}
+
+resource "aws_route_table" "interview_igw_rtb" {
+  vpc_id = aws_vpc.interview_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.interview_igw.id
+  }
+
+  tags = {
+    Name = "internet-igw-rtb"
+  }
+}
+
+resource "aws_route_table_association" "interview_public_subnet_a" {
+  subnet_id       = aws_subnet.interview_public_subnet_a.id
+  route_table_id  = aws_route_table.interview_igw_rtb.id
+}
+
+resource "aws_route_table_association" "interview_public_subnet_b" {
+  subnet_id       = aws_subnet.interview_public_subnet_b.id
+  route_table_id  = aws_route_table.interview_igw_rtb.id
+}
+
+resource "aws_route_table_association" "interview_public_subnet_c" {
+  subnet_id       = aws_subnet.interview_public_subnet_c.id
+  route_table_id  = aws_route_table.interview_igw_rtb.id
+}
